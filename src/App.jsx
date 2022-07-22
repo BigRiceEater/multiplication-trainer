af127e3ca2513591fa5d5cf628b30c4183f6b4ed
Big Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-07-22 09:33:40 +0800

    Add rudimentary routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,39 @@ import logo from "./logo.svg";
 import "./App.css";
 import Clock from "./components/Clock";
 import Trainer from "./components/Trainer";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from "react";
+
+function Settings() {
+  return (
+    <div>
+      <h1>This is the settings page</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Trainer />
-        <Clock />
+
+        <Router>
+          <Link to="/settings">Settings</Link>
+          <Link to="/">Trainer</Link>
+          <Routes>
+            <Route path="/settings" element={<Settings />} />
+            <Route
+              path="/"
+              element={
+                <React.Fragment>
+                  <Trainer />
+                  <Clock />
+                </React.Fragment>
+              }
+            />
+          </Routes>
+        </Router>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
