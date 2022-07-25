2ba14d1ea6c57c4284230cfd0bbcb20b6110a8d5
Big Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-07-25 11:01:29 +0800

    Fix number not generating results inclusive of min and max values

--- src/util/getNumber.js
+++ src/util/getNumber.js
@@ -1,7 +1,9 @@
-// results return is inclusive of max variable argument
+// results return is inclusive of min, max variable argument
+// The floor ensures we include of the min in the range
+// The +1 on max ensures we include max in the range
 
 function getNumber(min, max) {
-  return Math.ceil(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
 export default getNumber;
