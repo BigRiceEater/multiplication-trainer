bbc9eaedf95564f0182cfff687dfb9fc1564142d
Big Rice <34292181+BigRiceEater@users.noreply.github.com>
2022-07-25 10:31:50 +0800

    Fix setting min increased the number cap

--- src/util/getNumber.js
+++ src/util/getNumber.js
@@ -1,7 +1,7 @@
 // results return is inclusive of max variable argument
 
 function getNumber(min, max) {
-  return Math.ceil(Math.random() * max + min);
+  return Math.ceil(Math.random() * (max - min) + min);
 }
 
 export default getNumber;
